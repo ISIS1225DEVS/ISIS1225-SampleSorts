04ed881a9f750cda226730131243d945e4c1c5c3
Marulix <m.ruizg@uniandes.edu.co>
2021-06-22 16:40:11 -0500

    Entrega Ejemplo - laboratorio 4

--- App/view.py
+++ App/view.py
@@ -82,6 +82,15 @@ def printBestBooks(books):
 
 catalog = None
 
+def printResults(ord_books, sample=10): 
+    size = lt.size(ord_books) 
+    if size > sample: print("Los primeros ", sample, " libros ordenados son:") 
+    i=1 
+    while i <= sample: 
+        book = lt.getElement(ord_books,i) 
+        print('Titulo: ' + book['title'] + ' ISBN: ' + book['isbn'] + ' Rating: ' + book['average_rating']) 
+        i+=1
+
 """
 Menu principal
 """
@@ -117,7 +126,8 @@ while True:
         size = input("Indique tamaño de la muestra: ")
         result = controller.sortBooks(catalog, int(size))
         print("Para la muestra de", size, " elementos, el tiempo (mseg) es: ",
-                                          str(result))
+                                          str(result[0]))
+        printResults(result[1])
 
     else:
         sys.exit(0)
